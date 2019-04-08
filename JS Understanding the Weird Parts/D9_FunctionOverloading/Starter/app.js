fcaa8cc6d9e233bffa949848f5f207ac44a94e77
Henry Doce <henrydoce@protonmail.com>
2019-04-08 19:47:33 -0400

    Completed lesson 40 Framework Aside: Function Overloading

---
+++ JS Understanding the Weird Parts/D9_FunctionOverloading/Starter/app.js
@@ -0,0 +1,24 @@
+function greet(firstname, lastname, language) {
+
+  language = language || 'en';
+
+  if (language === 'en') {
+    console.log('Hello ' + firstname + ' ' + lastname )
+  }
+
+  if (language === 'es') {
+    console.log('Hola ' + firstname + ' ' + lastname )
+  }
+
+}
+
+function greetEnglish(firstname, lastname) {
+  greet(firstname, lastname, 'en');
+}
+
+function greetSpanish(firstname, lastname) {
+  greet(firstname, lastname, 'es');
+}
+
+greet('Henry', 'Doce', 'en');
+greet('Henry', 'Doce', 'es');
