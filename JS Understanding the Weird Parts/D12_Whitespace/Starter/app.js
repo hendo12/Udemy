4fdf0c08d6e515d550f0d49e4192bedd49bd5c8a
Henry Doce <henrydoce@protonmail.com>
2019-04-08 20:03:09 -0400

    Completed lesson 43 Framework Aside: Whitespace

---
+++ JS Understanding the Weird Parts/D12_Whitespace/Starter/app.js
@@ -0,0 +1,19 @@
+var 
+    //first name of the person
+    firstName, 
+    
+    //last name of the person
+    lastName,
+    
+    //the language can be 'en' or 'es'
+    language;
+
+var person = {
+  //the first name
+  firstName: 'Henry', 
+
+  //the last name (always required)
+  lastName: 'Doce'
+}
+
+console.log(person);
